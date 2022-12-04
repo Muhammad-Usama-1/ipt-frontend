8f5b6bd6af3dd49a607ef5769c602f1f8253d0bb
Muhammad Usama <mdusama225@gmail.com>
2022-12-04 06:06:44 +0500

    Resolve bug Error in storing real time token

---
+++ src/hooks/UseToken.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+export function useTokenHook() {
+  const [token, setToken] = useState("");
+  //   const [password, setPassword] = useState("");
+  const [change, setChange] = useState(false);
+
+  React.useEffect(() => {
+    const loadSaved = async () => {
+      setToken(localStorage.getItem("token"));
+    };
+    loadSaved();
+  }, [token]);
+
+  const saveData = async (token) => {
+    setChange(!change);
+    await localStorage.setItem("token", token);
+  };
+
+  const removeData = async () => {
+    setChange(!change);
+    await localStorage.removeItem("token");
+  };
+
+  return {
+    token,
+    saveData,
+    removeData,
+  };
+}
