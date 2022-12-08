d1fe5dce4fc5cb8161eff1ceb1882a88f843c14f
Muhammad Usama <mdusama225@gmail.com>
2022-12-08 12:13:26 +0500

    refactor code

--- src/components/FreindsSidebar.js
+++ src/components/FreindsSidebar.js
@@ -1,13 +1,13 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import React, { useContext } from "react";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import { useMediaQuery } from "@mui/material";
 
 import UserCard from "./cards/UserCard";
 import "../styles/FriendsSidebar.css";
-import { toast } from "react-toastify";
-import { client } from "../api/client";
-import { Link } from "react-router-dom";
-import { useTokenHook } from "../hooks/UseToken";
+// import { toast } from "react-toastify";
+// import { client } from "../api/client";
+// import { useTokenHook } from "../hooks/UseToken";
 import FriendContext from "../context/friendContext";
 
 function FreindsSidebar({ fsidebar }) {
@@ -27,25 +27,9 @@ function FreindsSidebar({ fsidebar }) {
     fsidebar.current.classList.toggle("close");
   };
 
-  // Get friends
-  // const getFriendsList = useCallback(async () => {
-  //   try {
-  //     const { data } = await client.get("/users/friends", {
-  //       headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
-  //     });
-  //     setFriends(data.data);
-
-  //     // console.log(data);
-  //   } catch ({ response }) {
-  //     if (response && response.status >= 400 && response.status < 500) {
-  //       console.log(response.data.message);
-  //       toast.error(response.data.message);
-  //     }
-  //   }
-  // }, []);
-  useEffect(() => {
-    // getFriendsList();
-  }, [0]);
+  // useEffect(() => {
+  //   // getFriendsList();
+  // }, [0]);
   return (
     <div style={{ display: "flex" }}>
       <div onClick={runFunc} className="fsidebar-mobile">
@@ -65,6 +49,7 @@ function FreindsSidebar({ fsidebar }) {
               <ul className="menu-links">
                 {friends?.map((el) => (
                   <Link
+                    key={el._id}
                     className="move-to-friend-profile"
                     to={`/friend-profile/${el.to_user?._id}`}
                   >
